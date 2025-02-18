17d4100fa0bb77b2decf69f199a09ef5abd3e3df
Gabriel <52799759+CrazorTheCat@users.noreply.github.com>
2025-02-18 05:08:40 -0500

    added obf

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 local scripts = {
-    [18901165922] = ""
+    [18901165922] = "https://raw.githubusercontent.com/CrazorTheCat/scr/refs/heads/main/ptgo.lua"
 }
 
 loadstring(game:HttpGet(scripts[game.gameId]))()
